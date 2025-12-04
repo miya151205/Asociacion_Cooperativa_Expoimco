b53e45f42b7bb869d6a072c873b2cccddb21faf0
Keiner-png <kjdurana@ufpso.edu.co>
2025-12-04 17:31:03 -0500

    se crea la capa del service

--- src/service/FinanzaService.java
+++ src/service/FinanzaService.java
@@ -1,5 +1,23 @@
 package service;
 
 public class FinanzaService {
-    
+    private double inversiones = 0;
+    private double ganancias = 0;
+
+    // Registrar inversión
+    public void registrarInversion(double valor) {
+        inversiones += valor;
+    }
+
+    // Registrar ganancia
+    public void registrarGanancia(double valor) {
+        ganancias += valor;
+    }
+
+    // Mostrar datos
+    public void mostrarFinanzas() {
+        System.out.println("=== ESTADO FINANCIERO ===");
+        System.out.println("Total inversiones: " + inversiones);
+        System.out.println("Total ganancias: " + ganancias);
+    }
 }
